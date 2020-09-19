cf869dc8891094a012e30ca5f1ece376fadefb81
MatthewBWhite <matthew@mh-white.com>
2020-09-19 08:13:48 +0100

    Add buefy navbar
    Signed-off-by: MatthewBWhite <matthew@mh-white.com>

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Buefy from 'buefy'
 
+Vue.use(Buefy)
 import './assets/sass/main.scss'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false;
 
